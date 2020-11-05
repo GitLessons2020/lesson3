23fcfe0ee9634ca2f5db74a01d00f828450214b1
Valery Kretov <valera.kretov@gmail.com>
2020-11-05 22:50:56 +0300

    broke

--- program.py
+++ program.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Base action'
+    return 'Base actions'
 
 @app.route('/v2')
 def v2():
-    return 'Second action'
+    return 'First action'
