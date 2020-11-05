445727baf4a1f679e67224c241dcbf66faa2f2af
Valery Kretov <valera.kretov@gmail.com>
2020-11-05 23:28:57 +0300

    Fixes

--- program.py
+++ program.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Base actions'
+    return 'Base action'
 
 @app.route('/v2')
 def v2():
-    return 'First action'
+    return 'Second action'
