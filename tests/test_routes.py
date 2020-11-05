ffad826ba2a64440ab84fd847a8a060198752187
Edgar <edgarik25@mail.ru>
2020-11-05 16:06:01 +0300

    test updated

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,10 +3,10 @@ import pytest
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    assert res.data == b"Base action"
+    assert res.data == b"First action!"
 
 
 def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
-    assert res.data == b"Second action"
+    assert res.data == b"Second action!"
