dbaa6f6c6b95c61c7ae792054f13ab53a14dc4ca
Светлана <36624900+Bogyfobiya@users.noreply.github.com>
2020-11-06 13:15:41 +0300

    correct test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_bogy4(app, client):
+    res = client.get('/bogy4')
+    assert res.status_code == 200
+    assert res.data == b"i return something"
