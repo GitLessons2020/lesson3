5426ac270d658832672b09c668155818c75a50c9
Victor <victor.nguen@gmail.com>
2020-11-06 19:00:17 +0300

    unnecessary import removed

--- program.py
+++ program.py
@@ -2,17 +2,19 @@ import random
 import string
 
 from flask import Flask
-from idna import unicode
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Base action'
 
+
 @app.route('/v2')
 def v2():
     return 'Second action'
+
 
 @app.route('/de4d10ck')
 def nguen_victor():
