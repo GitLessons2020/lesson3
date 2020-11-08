09301b59aa393fa7473b79472f836c9e1b550097
VegetableX <alexey_drozhzin@mail.ru>
2020-11-08 21:00:51 +0300

    Wrong test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_VegetableX(app, client):
+    res = client.get('/Drozhzhin')
+    assert res.status_code == 200
+    assert res.data == b"Drozhzhin A.V."
