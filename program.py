8304462f17115794a5cb58833e50b469fbe538b8
Михаил Мигунов <migunoff.uplay@gmail.com>
2020-11-13 21:00:49 +0300

    Returned original text

--- program.py
+++ program.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Action number 1'
+    return 'Base action'
 
 @app.route('/v2')
 def v2():
-    return 'Action number 2'
+    return 'Second action'
