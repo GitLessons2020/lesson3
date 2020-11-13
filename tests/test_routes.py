da8dff6a23fc89c236f25589833c628f9f6fb71f
Yaroslav <yaroslav_yudin_1999@mail.ru>
2020-11-14 00:24:05 +0300

    New test from yudin

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_v3(app, client):
+    res = client.get('/v3')
+    assert res.status_code == 200
+    assert res.data == b"Yudin INBO-04-17"
